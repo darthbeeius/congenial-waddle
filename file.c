cd2c748c268cca1a5e2dd607660a5f0923adb709
darthbeeius <adityakrishnakotha@gmail.com>
2017-04-28 14:34:22 +0530

    A c code to make the labels file (a.out > x.txt)

---
+++ file.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(){
+    int i=0;
+    for(i=0;i<=1000;i++){
+        printf("frame%d(2).jpg 0\n",i);
+    }
+    for(i=1001;i<=2002;i++){
+        printf("frame%d(2).jpg 1\n",i);
+    }
+   /* for(i=1601;i<=8601;i++){
+        printf("frame%d(2).jpg 1\n",i);
+    }
+    for(i=8602;i<=13602;i++){
+        printf("frame%d(2).jpg 0\n",i);
+    }*/
+}
